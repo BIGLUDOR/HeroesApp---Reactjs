7e9037630a8619a11c39619d55c098baedbb30e6
Luis Armando Lopez <luis_149@live.com>
2022-08-11 14:48:19 -0700

    First Commit to HeroesApp heroescard

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,12 @@
+import { HeroesList } from "../components"
+
+
+export const DcPage = () => {
+  return (
+    <>
+      <h1>DC Comics</h1>
+
+      <HeroesList publisher='DC Comics'/>
+    </>
+  )
+}
---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,11 @@
+import { HeroesList } from "../components"
+
+export const MarvelPage = () => {
+  return (
+    <>
+      <h1>Maverl Comics</h1>
+
+      <HeroesList publisher='Marvel Comics' />
+    </>
+  )
+}
